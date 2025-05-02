1533f16fa0ebaea31f689259c71d26472035255c
StepuraSerhii <119071515+StepuraSerhii@users.noreply.github.com>
2025-05-02 18:24:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 // Підключення зовнішнього скрипта Ringostat для аналітики
-(async function() {
-  const blockedIPs = ['176.104.184.135'];
-  try {
-      const res = await fetch('https://api.ipify.org?format=json');
-      const ip = (await res.json()).ip;
-      if (blockedIPs.includes(ip)) return;
-
-      (function (d, s, u, e, p) {
-          p = d.getElementsByTagName(s)[0];
-          e = d.createElement(s);
-          e.async = 1;
-          e.src = u;
-          p.parentNode.insertBefore(e, p);
-      })(document, 'script', 'https://script.ringostat.com/v4/1b/1b754cb63e621f14d71ac9233d0ba04a7fd8a22a.js');
-
-  } catch (e) {}
-})();
+(function (d, s, u, e, p) {
+  p = d.getElementsByTagName(s)[0];
+  e = d.createElement(s);
+  e.async = 1;
+  e.src = u;
+  p.parentNode.insertBefore(e, p);
+})(document, 'script', 'https://script.ringostat.com/v4/1b/1b754cb63e621f14d71ac9233d0ba04a7fd8a22a.js');
 
 // Запускаємо функції після завантаження сторінки
 document.addEventListener("DOMContentLoaded", () => {
